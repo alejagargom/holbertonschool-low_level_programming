53a22c4f2859f2312d73fbf26507e490778ad85c
alejagargom <791@holbertonschool.com>
2019-02-06 21:17:58 +0000

    first commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+* main - prints a phrase for the purpose of demonstrating puts
+* Return: 0 upon printing of the phrase.
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
