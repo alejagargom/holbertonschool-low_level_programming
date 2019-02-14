abc5d16911ae434fb7aa2f3accb1f05f166b1252
alejagargom <791@holbertonschool.com>
2019-02-14 01:59:44 +0000

    comment

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,23 +1,21 @@
 #include "holberton.h"
 
 /**
- * print_alphabet_x10 - check the code for Holberton School students.
- *
+ * print_alphabet_x10 - Print with the lowercase code to z 10 times
  *
  */
 void print_alphabet_x10(void)
 {
-	char a;
-	int n = 1;
+	char lowercase_1;
+	int cont = 0;
 
-	while (n <= 10)
+	while (cont < 10)
 	{
-		(n++);
-
-		for (a = 'a' ; a <= 'z'; a++)
+		for (lowercase_1 = 97 ; lowercase_1 <= 122; lowercase_1++)
 		{
-			_putchar(a);
+			_putchar(lowercase_1);
 		}
+		cont++;
 		_putchar('\n');
 	}
 }
