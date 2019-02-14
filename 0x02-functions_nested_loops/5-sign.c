65cb210fbf205d6d7050fd8a346e4c41a95699bc
alejagargom <791@holbertonschool.com>
2019-02-14 03:19:26 +0000

    comment

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,26 +1,26 @@
 #include "holberton.h"
 /**
- * print_sign - sense if n is grater less or equal than 0
+ * print_sign - sense if mom is grater less or equal than 0
+ * @MOm: I check if Mm is equal to or greater than is zero or is less than 0
  *
- * @n: is a char to be evaluated
- *
- * Return: 1 if is n is grater less or equal than 0
+ * Return: 1-positive 0-zero -1-negative
  */
-int print_sign(int n)
+int print_sign(int MOm)
 {
-	if (n > 0)
-	{
-		_putchar('+');
-		return (1);
-	}
-	else if (n == 0)
-	{
-		_putchar('0');
-		return (0);
-	}
-	else
+	if (MOm < 0)
 	{
 		_putchar('-');
 		return (-1);
 	}
+	if (MOm == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	if (MOm > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	return (2);
 }
