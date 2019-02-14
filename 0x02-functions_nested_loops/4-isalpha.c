29abe5f3eabf37af1b7963c84826afe4eace931c
alejagargom <791@holbertonschool.com>
2019-02-14 02:47:46 +0000

    comment

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,14 @@
 #include "holberton.h"
 /**
- * _isalpha - sense if the char is lowcase
+ * _isalpha - checks Mm
+ * @Mm:my variable Mm is greater than or equal to 97 and less than 122
+ * in minuscule and the same in upper case Mm 65 to 90
  *
- * @c: is a char to be evaluated
- *
- * Return: 1 if is lowercase 0 if not
+ * Return: 1 if is Mm 0 if not
  */
-int _isalpha(int c)
+int _isalpha(int Mm)
 {
-	return (((c >= 97 && c <= 122) || (c >= 65 && c <= 90)) ? 1 : 0);
+	if ((Mm >= 97 &&  Mm <= 122) || (Mm >= 65 && Mm <= 90))
+		return (1);
+	return (0);
 }
