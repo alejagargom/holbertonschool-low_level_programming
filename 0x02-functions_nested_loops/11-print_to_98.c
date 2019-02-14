034774cbfddc2207d0b5c2690a373a2e3b6c7077
alejagargom <791@holbertonschool.com>
2019-02-14 06:29:00 +0000

    commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,10 +2,21 @@
 #include "holberton.h"
 
 /**
- * print_to_98 -
+ * print_to_98 if it is less it is added if it is greater it is subtracted
  * @luna:
  */
 void print_to_98(int luna)
 {
-
+	for (; luna < 98; luna++)
+	{
+		printf("%i, ", luna);
+	}
+	for (; luna >= 98; luna--)
+	{
+		if (luna == 98)
+			printf("98");
+		else
+			printf("%i, ", luna);
+	}
+	printf("\n");
 }
