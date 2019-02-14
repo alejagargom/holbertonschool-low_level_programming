48cebf55959ee3a10c275705fcb4e884be8cef82
alejagargom <791@holbertonschool.com>
2019-02-14 23:03:54 +0000

    commit

---
+++ 0x03-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _isdigit- retunr d.
+ *@d: checking if d is digit
+ *
+ * Return: d in digit 1 and 0;
+ */
+int _isdigit(int d)
+{
+	if (d >= 48  &&  d <= 57)
+		return (1);
+
+	else
+	return (0);
+
+}
