8bcd5d0583ea95aaee4cee2fe7927ae6b11f325c
alejagargom <791@holbertonschool.com>
2019-02-18 06:38:53 +0000

    first commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,45 +1,39 @@
-#include <stdio.h>
 #include "holberton.h"
 
 /**
- * print_to_98 - prints all natural numbers from n to 98 with a newline
- *
- * @n: the starting point for the printing
- *
- * Return: none
+ * print_to_98 - print list nuber to 98.
+ * @n: startpoint to list
  */
 void print_to_98(int n)
 {
-	int i;
-
-	if (n < 98)
+	while (n < 98 || n > 98)
 	{
-		for (i = n; i <= 98; i++)
-		{
-			printf("%d", i);
-
-			if (i != 98)
-			{
-				printf(",");
-				printf(" ");
-			}
-		}
-		printf("\n");
-	} else if (n > 98)
+		print_number(n);
+		_putchar(',');
+		_putchar(32);
+		n < 98 ? n++ : n--;
+	}
+	print_number(n);
+	_putchar(10);
+}
+/**
+ * print_number - print number with _putchar in char format
+ * @number: number to be printed
+ */
+void print_number(int number)
+{
+	if (number < 10 && number >= 0)
 	{
-		for (i = n; i >= 98; i--)
-		{
-			printf("%d", i);
-
-			if (i != 98)
-			{
-				printf(",");
-				printf(" ");
-			}
-		}
-		printf("\n");
-	} else
+		_putchar(number + '0');
+	}
+	else if (number < 0)
 	{
-		printf("%d\n", n);
+		_putchar('-');
+		print_number(-number);
+	}
+	else
+	{
+		print_number(number / 10);
+		print_number(number % 10);
 	}
 }
