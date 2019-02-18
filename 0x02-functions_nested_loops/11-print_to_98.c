2c7d9a92c5833a6b6f7acca94463527aca13da7a
alejagargom <791@holbertonschool.com>
2019-02-18 06:21:19 +0000

    first commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,18 +9,31 @@
  */
 void print_to_98(int n)
 {
-	while (n < 98)
-	{
-		printf("%d, ", n);
-		n++;
-	}
-	while (n > 98)
-	{
-		printf("%d, ", n);
-		n--;
-	}
-	if (n == 98)
-	{
-		printf("%d\n", n);
-	}
+        if (n == 98)
+        {
+                printf("%d", n);
+        }
+        else if (n < 98)
+        {
+                for (; n <= 98; n++)
+                {
+                        printf("%d", n);
+                        if (n != 98)
+                        {
+                                printf(", ");
+                        }
+                }
+        }
+        else
+        {
+                for (; n >= 98; n--)
+                {
+                        printf("%d", n);
+                        if (n != 98)
+                        {
+                                printf(", ");
+                        }
+                }
+        }
+        printf("\n");
 }
