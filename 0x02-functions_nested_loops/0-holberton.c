9f504332d4c56a6704969af66d5efddc40e9dd34
alejagargom <791@holbertonschool.com>
2019-02-18 03:25:07 +0000

    first commit

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,11 +1,14 @@
 #include "holberton.h"
 int main(void)
 {
-char a[] = "Holberton\n";
-int i;
-for (i = 0; i < 10; i++)
-{
-_putchar(a[i]);
-}
+_putchar('H');
+_putchar('o');
+_putchar('l');
+_putchar('b');
+_putchar('e');
+_putchar('r');
+_putchar('t');
+_putchar('o');
+_putchar('n');
 return (0);
 }
