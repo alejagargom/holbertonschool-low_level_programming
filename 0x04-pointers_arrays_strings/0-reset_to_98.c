8614d6a2019aa1baf6686431ec7dc74f52b526e4
alejagargom <791@holbertonschool.com>
2019-02-20 02:34:15 +0000

    comment

---
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *reset_to_98 - asks me the function of the pointer and that of the value to the address of n
+ *
+ *@n:the variable n equals 98
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
