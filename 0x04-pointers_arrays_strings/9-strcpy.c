60d04003bdf57b486e6edc2885596bac1fa1eaa0
alejagargom <791@holbertonschool.com>
2019-02-21 19:58:02 +0000

    first commit

--- 0x04-pointers_arrays_strings/9-strcpy.c
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -13,7 +13,6 @@ char *_strcpy(char *dest, char *src)
 	do {
 		*(dest + cp) = *(src + cp);
 		cp++;
-	}
-while (*(src + cp - 1) != '\0');
-		return (dest);
+	} while (*(src + cp - 1) != '\0');
+	return (dest);
 }
