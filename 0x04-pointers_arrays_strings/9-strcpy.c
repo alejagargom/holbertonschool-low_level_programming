bd96e17439737368ce4c6d52bc8856f7febbd714
alejagargom <791@holbertonschool.com>
2019-02-21 19:21:36 +0000

    first commit

--- 0x04-pointers_arrays_strings/9-strcpy.c
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -1,20 +1,20 @@
 #include "holberton.h"
 /**
- * _strcpy - copy src string to dest string
+ * _strcpy - copies src string to dest string
  * @dest: destination string
  * @src: source string
  * Return: no exist
  */
 char *_strcpy(char *dest, char *src)
 {
-	int lth = 0, r;
+	int lth = 0, o;
 
 	while (src[lth])
 		lth++;
 
-	for (r = 0; r <= lth - 1; r++)
+	for (o = 0; o <= lth - 1; o++)
 	{
-		dest[r] = src[r];
+		dest[o] = src[o];
 	}
 
 	return (dest);
